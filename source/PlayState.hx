c692cd51368e96001439fe9d32fdd7046f0c2be2
Jamboger <jamboger@gmail.com>
2018-01-27 00:49:41 +0000

    first commit - hello world

---
+++ source/PlayState.hx
@@ -0,0 +1,19 @@
+package;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	override public function create():Void
+	{
+		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
+		text.screenCenter();
+		add(text);
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
